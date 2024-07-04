9a216ea4282b73787e70aaad7b34dd0d1138c03c
Riday1 <rrriday100@gmail.com>
2024-07-04 22:45:21 +0600

    services section is fully completed,

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -15,6 +15,10 @@ const Services = () => {
 
     return (
         <section className='my-20'>
+            <div className='my-10'>
+                <p className='text-gray-400 text-lg text-center font-bold my-4'>Service We Provide</p>
+                <h1 className='text-6xl font-bold text-indigo-500 text-center'>Explore Our Services</h1>
+            </div>
             <div className=' my-20 grid grid-cols-1 md:gird-cols-2 lg:grid-cols-3 gap-10 px-20'>
 
                 {
